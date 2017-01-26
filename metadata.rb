b31e0d6ed43aa8bd15fc6b016dcb5e831cb5ed87
vijay-thakur <vijay-thakur@users.noreply.github.com>
2017-01-26 15:36:25 +0530

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'cassandra-multinode'
-maintainer       'Jagatveer Singh'
-maintainer_email 'jagatveer@hotmail.com'
+maintainer       'Vijay Singh'
+maintainer_email 'abc@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures cassandra-multinode'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
